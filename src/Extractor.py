a2f4a18028c3ed5dda62f273b48ee533251f7939
raquelaalvarez <raquel.alvarez.gomez-calcerrad@estudiantat.upc.edu>
2026-03-09 18:44:47 +0100

    changing phases names

---
+++ src/Extractor.py
@@ -0,0 +1,66 @@
+# Renderize the html to get a suitable input for the extractor (html + image(s)
+from playwright.sync_api import sync_playwright
+
+
+def screenshot_chart(url: str, out_path: str):
+    # "Chart detection based on visual prominence heuristics (largest SVG element in DOM)."
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page(viewport={"width": 1440, "height": 900})
+
+        page.goto(url, wait_until="networkidle")
+        page.wait_for_timeout(3000)
+
+        svgs = page.locator("svg")
+        count = svgs.count()
+
+        if count == 0:
+            raise RuntimeError("No SVGs found")
+        # print(count)
+        largest = None
+        largest_area = 0
+
+        for i in range(count):
+            el = svgs.nth(i)
+            box = el.bounding_box()
+            if box:
+                area = box["width"] * box["height"]
+                if area > largest_area:
+                    largest_area = area
+                    largest = el
+
+        if largest is None:
+            raise RuntimeError("No visible SVG found")
+
+        largest.scroll_into_view_if_needed()
+        largest.screenshot(path=out_path)
+
+        browser.close()
+
+def debug_page(url: str):
+    # y si nos quedasemos solo con la captura de la página web?
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page(viewport={"width": 1440, "height": 900})
+        page.goto(url, wait_until="domcontentloaded")
+        page.wait_for_timeout(3000)  # deja respirar el JS
+
+        page.screenshot(path="fullpage.png", full_page=True)
+        html = page.content()
+        with open("page.html", "w", encoding="utf-8") as f:
+            f.write(html)
+
+        browser.close()
+
+
+
+# Example usage
+if __name__ == "__main__":
+    debug_page("https://hackupc.com/") # checks the page and creates fullpage.png to see what the web page is looking like.
+
+
+    screenshot_chart(
+        "https://hackupc.com/",
+        "chart.png",
+    
+    )
